3483db41b9e140cf79806d2bf7285e96301a813a
RozWhite <77862612+RozWhite@users.noreply.github.com>
2022-07-10 12:57:09 -0400

    first commit

---
+++ app.py
@@ -0,0 +1,64 @@
+
+from flask import Flask, request
+import numpy as np
+import pickle
+import pandas as pd
+from flasgger import Swagger
+
+
+
+app=Flask(__name__)
+Swagger(app)
+
+pickle_in = open("lg.pkl","rb")
+classifier=pickle.load(pickle_in)
+
+@app.route('/')
+def welcome():
+    return "Welcome All"
+
+@app.route('/predict',methods=["Get"])
+def predict():
+    
+    """Let's predict the species of Iris flowers:[ 0: 'setosa',1: 'versicolor', 2: 'virginica' ]  
+  Using ml
+    ---
+    parameters:  
+      - name: sepal_length
+        in: query
+        type: number
+        required: true
+      - name: sepal_width
+        in: query
+        type: number
+        required: true
+      - name: petal_length
+        in: query
+        type: number
+        required: true
+      - name: petal_width
+        in: query
+        type: number
+        required: true
+    responses:
+        200:
+            description: The output values
+        
+    """
+    
+    sepal_length=request.args.get("sepal_length")
+    sepal_width=request.args.get("sepal_width")
+    petal_length=request.args.get("petal_length")
+    petal_width=request.args.get("petal_width")
+
+    input_data=np.array([[sepal_length, sepal_width, petal_length, petal_width]],dtype=np.double)
+    prediction=classifier.predict(input_data)
+    #prediction=classifier.predict([[sepal_length, sepal_width, petal_length, petal_width]])
+
+    return "Hello The answer is"+str(prediction)
+
+
+
+if __name__=='__main__':
+    app.run(debug=True)
+    
